368cbdfe57249241c647903589ef68edb9c749a4
Akbar Haryadi <akbarharyadi777@gmail.com>
2022-12-29 21:06:27 +0700

    data

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -19,7 +19,7 @@
                                     <div class="text-center">
                                         <p class="mb-4">Silahkan login dengan akun google Anda.</p>
                                         <div class="flex justify-center pt-1 mb-1 pb-1">
-                                            <a href="http://localhost:4000/auth/google" data-mdb-ripple="true"
+                                            <a href="http://www.bitmerce.cloud:5000/auth/google" data-mdb-ripple="true"
                                                 data-mdb-ripple-color="light"
                                                 class="text-black flex items-center inline-block px-12 py-2.5 mb-2 font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
                                                 style="background-color: #8ad300;">
@@ -27,17 +27,17 @@
                                                     class="w-4 h-4">
                                                     <path fill="currentColor"
                                                         d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                                                </svg> + Masuk dengan Google Akun (Perorangan)
+                                                </svg> + Masuk dengan Google Akun
                                             </a>
                                         </div>
-                                        <div class="flex justify-center pt-1 mb-1 pb-1">
+                                        <!-- <div class="flex justify-center pt-1 mb-1 pb-1">
                                             <button type="button" data-mdb-ripple="true"
                                                 data-mdb-ripple-color="light"
                                                 class="text-black flex items-center inline-block px-12 py-2.5 mb-2 font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
                                                 style="background-color: #8ad300;">
                                                 Masuk atau Daftar (Kelompok)
                                             </button>
-                                        </div>
+                                        </div> -->
                                         <p class="mb-4">Anda akan dialihkan dari halaman ini untuk login dengan akun
                                             google Anda.</p>
                                     </div>
